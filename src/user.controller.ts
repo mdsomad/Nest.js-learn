48e2e933fb63b5925d3f79c9be0c9633c21b27dd
Md Somad <103892160+mdsomad@users.noreply.github.com>
2023-10-09 20:40:16 +0530

    complete

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,50 +1,31 @@
-import { Controller, Post, Body, Param, Get, Put, Delete } from '@nestjs/common';
-import { CreateUserDTO } from './dto';
+import { Controller ,Get ,HostParam,Ip} from '@nestjs/common';
 
-let USERS = [];
-
-@Controller('users')               //* <-- Iska naam hai decorator
+@Controller('users')                                                  //* <-- Iska naam hai decorator
+//* @Controller({path:'/users',host: 'app:domain.com'})               //* <-- Iska naam hai decorator
 export class Usercontroller {
 
 
-  //TODO Post Methods Used
-  @Post()
-  addUser(@Body() createUserDto: CreateUserDTO) {
-    USERS.push(createUserDto);
-    return 'User added successfully';
-  }
-
-  //TODO Get Methods Used
-  @Get()
-  getUsers() {
-    return USERS;
-  }
 
 
-   //TODO Get Methods Used
-  @Get(':id')
-  getUser(@Param('id') id: number) {
-    return USERS.find((user) => +user.id === +id);
-  }
+    //TODO Get Methods Used Find ip
+    @Get()
+    getUsers(@Ip() ip: string) {
+      console.log(ip);
+      return 'hello world';
+    }
+  
   
 
-   //TODO Put & Update Methods Used
-  @Put(':id')
-  updateUser(@Param('id') id: number, @Body() updateUserDto: CreateUserDTO) {
-    const userIdx = USERS.findIndex((user) => +user.id === +id);
 
-    if (!userIdx) {
-      return;
-    }
 
-    USERS[userIdx] = updateUserDto;
+  //TODO Get Methods Used  Find domain
+  @Get()
+  getUsers2(@HostParam('domain') params: Record<string,any>) {
+    return 'hello world 2';
   }
 
-  //TODO Delete Methods Used
-  @Delete(':id')
-  deleteUser(@Param('id') id: number) {
-    USERS = USERS.filter((user) => +user.id !== +id);
-  }
+
+
 
 
 
