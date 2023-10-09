ee267a99f392677c536cc8288874865eced90763
Md Somad <103892160+mdsomad@users.noreply.github.com>
2023-10-09 16:20:58 +0530

    DTO

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,56 +1,22 @@
 import {
   Controller,
-  Get,
-  Param,
-  Query,
-  Headers
+  Post,
+  Body
 } from '@nestjs/common';
 
 
-interface videosparams {
-  id:number,
-  name:string
-}
-interface queryParam {
-  age:number,
-  name:string
-}
 
 
 @Controller('users') //* <-- Iska naam hai decorator
 export class Usercontroller {
-  @Get('/videos/:id/:name')
-  getVideos(@Param() params:videosparams) {
-    console.log(params);
-    return 'Success';
+  @Post('/video')
+  addVideo(@Body() requestData:Record<string,any>) {
+    console.log(requestData);
+    return {
+      success:true
+    };
   }
 
-
-  //* no data type defined params
-  // @Get('/videos/:id')
-  // getVideos(@Param() params:Record<string,any>) {
-  //   console.log(params);
-  //   return 'Success';
-  // }
-
-
-
-   //* QueryParam use
-  @Get('/videos2')
-  getVideos2(@Query() query:queryParam) {
-    console.log(query);
-    console.log(query.name);
-    return 'Success';
-  }
-
-
-
-   //* headers data get
-  @Get('/headers')
-  getHeaders(@Headers() headers:Record<string,any>) {
-    console.log(headers);
-    return 'Success';
-  }
   
   
 
