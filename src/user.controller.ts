c569857f1fa4e9b0eaf7092129607a57e1471071
Md Somad <103892160+mdsomad@users.noreply.github.com>
2023-10-09 20:18:49 +0530

    crud operation complete

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,30 +1,52 @@
-import {
-  Controller,
-  Post,
-  Body
-} from '@nestjs/common';
+import { Controller, Post, Body, Param, Get, Put, Delete } from '@nestjs/common';
+import { CreateUserDTO } from './dto';
 
+let USERS = [];
 
-
-
-@Controller('users') //* <-- Iska naam hai decorator
+@Controller('users')               //* <-- Iska naam hai decorator
 export class Usercontroller {
-  @Post('/video')
-  addVideo(@Body() requestData:Record<string,any>) {
-    console.log(requestData);
-    return {
-      success:true
-    };
+
+
+  //TODO Post Methods Used
+  @Post()
+  addUser(@Body() createUserDto: CreateUserDTO) {
+    USERS.push(createUserDto);
+    return 'User added successfully';
   }
 
+  //TODO Get Methods Used
+  @Get()
+  getUsers() {
+    return USERS;
+  }
+
+
+   //TODO Get Methods Used
+  @Get(':id')
+  getUser(@Param('id') id: number) {
+    return USERS.find((user) => +user.id === +id);
+  }
   
-  
+
+   //TODO Put & Update Methods Used
+  @Put(':id')
+  updateUser(@Param('id') id: number, @Body() updateUserDto: CreateUserDTO) {
+    const userIdx = USERS.findIndex((user) => +user.id === +id);
+
+    if (!userIdx) {
+      return;
+    }
+
+    USERS[userIdx] = updateUserDto;
+  }
+
+  //TODO Delete Methods Used
+  @Delete(':id')
+  deleteUser(@Param('id') id: number) {
+    USERS = USERS.filter((user) => +user.id !== +id);
+  }
+
+
 
 
 }
-
-
-
-
-
-
