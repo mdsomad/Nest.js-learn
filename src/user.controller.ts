08fceb4e9ba836857ae378d058db33ee3c3746fb
Md Somad <103892160+mdsomad@users.noreply.github.com>
2023-10-09 11:56:55 +0530

    response complete

--- src/user.controller.ts
+++ src/user.controller.ts
@@ -1,14 +1,76 @@
-import { Controller, Get, Post, Put, Delete, Patch, Req } from '@nestjs/common';
-import { Request } from 'express';
+import {
+  Controller,
+  Post,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Req,
+  Res,
+  Header,
+  Redirect
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 
-@Controller('users')               //* <-- Iska naam hai decorator
+// @Controller('users') //* <-- Iska naam hai decorator
+// export class Usercontroller {
+//   @Post('profile')
+//   // @HttpCode(200)
+//   @HttpCode(HttpStatus.ACCEPTED)
+//   getProfile(@Req() req: Request, @Res({ passthrough: true }) resp: Response) {
+
+//     resp.status(200)
+//     return {
+//       Hello: ' Somad',
+//     };
+//   }
+// }
+
+
+
+
+
+
+//TODO: Set Header
+@Controller('users') //* <-- Iska naam hai decorator
 export class Usercontroller {
-  @Get('profile')
+  @Post('profile')
+  // @Header('Cache-Control',"none")
+  // @Header('X-Name',"somad"),
+  @Redirect('/users/account',302)       //* <-- Static Route Use This Method  
   getProfile(@Req() req: Request) {
-    console.log(req);
 
-    return {
-      Hello: ' Somad',
-    };
+    //* Dynamically switch Route Use This method
+    const rn = (Math.random()* 10 + 1);
+
+    console.log(rn)
+    
+    if(rn < 5 ){
+      return {
+        url: '/users/account',
+        statusCode:302
+      }
+    }else{
+      return {
+        url: '/users/wallet',
+        statusCode:302
+      }
+    }
+    
+    
+  }
+
+
+
+
+  @Get('/account')
+  redirectRoute(){
+    console.log('Working account called')
+     return "Working account";
+  }
+
+  @Get('/wallet')
+  redirectWallet(){
+    console.log('Working wallet called')
+     return "Working wallet";
   }
 }
